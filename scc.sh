46d1564043f2ab3e6c2d513a8421c65f71f1669c
angel-paz <angel.paz@kyndryl.com>
2023-09-28 16:21:07 +0200

    test

---
+++ scc.sh
@@ -0,0 +1,14 @@
+comment_url="https://api.github.com/repos/apazg7/myapp-src/issues/7/comments"
+
+token="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"
+
+trivy_report=$(trivy image apazg7/myapp:1.0.1 --format json | jq -r '.Results[1].Vulnerabilities[] | "\(.VulnerabilityID), \(.PkgName) \(.InstalledVersion)\n<br>"' | xargs echo )
+
+temp_file=$(mktemp)
+
+echo "{\"body\": \"${trivy_report}\"}" > "${temp_file}"
+
+cat "${temp_file}"
+
+curl -s -X POST -H "Authorization: token ${token}" -d "@${temp_file}" "${comment_url}"
+
